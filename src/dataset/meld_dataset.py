23830fba96daeeaf5ab8283fead18f996009e56b
Nathan Gabriel <nategabrielk@icloud.com>
2026-06-18 21:34:09 -0400

    setup dataset directory

---
+++ src/dataset/meld_dataset.py
@@ -0,0 +1,15 @@
+from torch.utils.data import Dataset
+
+
+
+
+class MELDDataset(Dataset):
+
+    def __init__(self):
+        print("hello")
+
+
+if __name__ == "__main__":
+    dataset = MELDDataset()
+
+    
